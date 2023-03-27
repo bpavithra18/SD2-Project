2b8a43be4dc65dc179c4172053d6a6f79a012b2b
sai revanth reddy <sairevanthreddy24@gmail.com>
2023-03-27 12:46:14 +0100

    added database

--- app/app.js
+++ app/app.js
@@ -96,58 +96,9 @@ app.get("/user-property/:id", async function (req, res) {
     await property.getPropertyDetails();
     console.log(property);
     res.render('property', {property:property});
-/*
-// Task 3 single programme page
-app.get("/user-single/:id", function (req, res) {
-    var pCode = req.params.Name;
-    output = '';
-    output += "<h1>Programme</h1>";
-   
-    //Now call the database for the modules
-    //Why do you think that the word modules is coming in before the name of the programme??
-    var modSql = "SELECT * FROM users \
-    WHERE Name = ?";
-    output += "<h2>phone</h2>";
-    db.query(modSql, [pCode]).then(results => {
-        output += '<table border="1px">';
-        for (var row of results) {
-            output += '<tr>';
-            output += '<td><a href="/user-single/">' + row.name + '</a></td>';
-            output += '<td>' + row.id + '</td>';
-            output += '</tr>'
-        }
-        output+= '</table>';
-        res.send(output);    
-    });
 
 });
-*/
-// Create a route for testing the db
-app.get("/db_test1", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from user_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-});
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
 
 // Start server on port 3000
 app.listen(3000,function(){
