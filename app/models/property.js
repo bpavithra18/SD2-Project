ac1addf8485d6ecb14f40b9d7e08b99edf3306ba
kishore1278 <yuvadonadi17@gmail.com>
2023-03-27 16:08:58 +0530

    sinple property details updated

---
+++ app/models/property.js
@@ -0,0 +1,29 @@
+// Get the functions in the db.js file to use
+const db = require('./../services/db');
+
+class User {
+    // property ID
+    id;
+    // property location
+    location;
+
+
+    constructor(id) {
+        this.id = id;
+    }
+    
+    // Gets the student name from the database
+    async getPropertyDetails() {
+        if (typeof this.location !== 'string') {
+            var sql = "SELECT * from property where id = ?"
+            const results = await db.query(sql, [this.id]);
+            this.location = results[0].location;
+        }
+
+    }
+
+}
+
+module.exports = {
+    User
+}
